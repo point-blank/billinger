6c27cf7e1667223f0055c9b10f10bc7cba62bc6c
grigorij2503 <g.kolaski@gmail.com>
2014-09-26 15:37:30 +0200

    First app tests

---
+++ app/src/androidTest/java/billinger/blank/point/pl/billinger/ApplicationTest.java
@@ -0,0 +1,13 @@
+package billinger.blank.point.pl.billinger;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/billinger/point/blank/pl/billinger/activities/BillsListActivity.java
@@ -0,0 +1,19 @@
+package billinger.point.blank.pl.billinger.activities;
+
+import android.os.Bundle;
+
+import billinger.blank.point.pl.billinger.R;
+import roboguice.activity.RoboActivity;
+import roboguice.inject.ContentView;
+
+/**
+ * Main Activity
+ * Created by gko on 2014-09-26.
+ */
+@ContentView(R.layout.main)
+public class BillsListActivity extends RoboActivity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
